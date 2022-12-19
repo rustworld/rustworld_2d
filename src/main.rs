ac3d1c2811cca6bb347046423d01cb1af85b399c
rustypat <none@none.org>
2022-12-19 16:15:58 +0100

    graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 use rustworld_2d;
+use tokio::signal;
+use futures_util::future::{self, Either, FutureExt};
 
 
-fn main() {
-    let web_server_adress = "127.0.0.1:5000";
-
+#[tokio::main]
+async fn main() {
+    // say hello
     println!("\nHello, rust world!\n");
+
+    // configer web server
+    let web_server_adress = "127.0.0.1:5000";
+    let server_future = gotham::init_server(web_server_adress, ||Ok(rustworld_2d::say_hello));
+
+    // configer Ctrl-C future
+    let ctrl_c_future = async {
+        signal::ctrl_c().await.expect("failed to listen for Ctrl-C");
+        println!("Ctrl-C pressed.");
+    };
+
+    // start web server and Ctrl-C handler
     println!("Start Gotham Web Server. Listening at {}", web_server_adress);
-    gotham::start(web_server_adress, ||Ok(rustworld_2d::say_hello)).expect("failed to start gotham web server");
+    let exit_reason = future::select(server_future.boxed(), ctrl_c_future.boxed()).await;
+
+    // exit program
+    if let Either::Left((Err(err), _ )) = exit_reason {
+        println!("Error starting web server: {}", err);
+    } else if let Either::Right(_) = exit_reason {
+        println!("web server closed");
+    } else {
+        println!("unknown exit reson! What the heck?");
+    }
+
 }
 
 
